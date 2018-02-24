e8d90dc90a6ccb40433f7bf1733ad67488109d51
Brice Theurillat <brice.theurillat@outlook.com>
2018-02-24 13:59:25 +0100

    Wait for template to be ready before sending an email

--- src/Mailer.ts
+++ src/Mailer.ts
@@ -1,6 +1,6 @@
 import { MailTemplate, TemplateOptions } from "./MailTemplate";
 
-import Bluebird from "bluebird";
+import * as Bluebird from "bluebird";
 import * as nodemailer from "nodemailer";
 
 interface TemplateObject {
@@ -34,7 +34,7 @@ export class Mailer {
      * Sends a mail 
      * @param mailOptions Mail Options
      */
-    public sendMail(mailOptions: nodemailer.SendMailOptions);
+    public sendMail(mailOptions: nodemailer.SendMailOptions): PromiseLike<nodemailer.SentMessageInfo>;
 
     /**
      * Sends a mail using a template
@@ -43,24 +43,36 @@ export class Mailer {
      * @param context Context used to compute the template
      */
     public sendMail(mailOptions: nodemailer.SendMailOptions, templateName?: string, context?: any): PromiseLike<nodemailer.SentMessageInfo> {
-        return new Bluebird<nodemailer.SentMessageInfo>((resolve, reject) => {
+        return new Bluebird<nodemailer.SentMessageInfo>((resolve: Function, reject: Function) => {
+            let options = {
+                ...mailOptions
+            }; 
             if (templateName) {
                 const template = this.templates[templateName];
                 if (template === undefined) {
                     reject(new Error("Wrong template name"));
                     return;
                 } else {
-                    mailOptions = {
-                        ...template.defaults,
-                        ...template.template.compute(context),
-                        ...mailOptions
-                    };
+                    if (template.template.isReady()) {
+                        mailOptions = {
+                            ...template.defaults,
+                            ...template.template.compute(context),
+                            ...mailOptions
+                        };
+                    } else {
+                        template.template.once("ready", () => {
+                            mailOptions = {
+                                ...template.defaults,
+                                ...template.template.compute(context),
+                                ...mailOptions
+                            };
+                        });
+                    }
                 }
-
-                this.transporter.sendMail(mailOptions, (err: Error | null, info: nodemailer.SentMessageInfo) => {
-                    err ? reject(err) : resolve(info);
-                });
             }
+            this.transporter.sendMail(mailOptions, (err: Error | null, info: nodemailer.SentMessageInfo) => {
+                err ? reject(err) : resolve(info);
+            });
         });
     }
 }
