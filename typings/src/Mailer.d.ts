541d103bb4a06c48fbcb387d01017d7e496e6bb9
Brice Theurillat <brice.theurillat@outlook.com>
2018-02-24 15:26:39 +0100

    Add typings

---
+++ typings/src/Mailer.d.ts
@@ -0,0 +1,25 @@
+import { TemplateOptions } from "./MailTemplate";
+import * as nodemailer from "nodemailer";
+export interface SendMailOptions extends nodemailer.SendMailOptions {
+    template?: {
+        name: string;
+        context: any;
+    };
+}
+export declare class Mailer {
+    private templates;
+    private transporter;
+    constructor(options: any);
+    /**
+     * Creates a template
+     * @param name Name of the template
+     * @param template Template
+     */
+    createTemplate(name: string, template: TemplateOptions, defaults: nodemailer.SendMailOptions): void;
+    /**
+     * Sends a mail
+     * @param mailOptions Mail Options
+     */
+    sendMail(mailOptions: SendMailOptions): PromiseLike<nodemailer.SentMessageInfo>;
+    private send(mailOptions, resolve, reject);
+}
